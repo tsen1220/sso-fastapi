5431cad73582c57365fab7b30212314b3ed99efd
Ken Tseng <tsen1220.us@gmail.com>
2025-02-16 23:25:09 +0800

    feat: add config.

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "mysql+pymysql://username:password@localhost/dbname"
+from app import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
